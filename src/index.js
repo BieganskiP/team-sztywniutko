29053c260e968b6bb0d6d219179e3971adf4ebce
EwaMazurek <ewa.mmazurek@gmail.com>
2023-02-09 12:27:39 +0100

    header done-ish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('test');
+
+import { mainHeaderDisplay } from './js/header';
+import { libraryHeaderDisplay } from './js/header';
+import { activateLibraryBtn } from './js/header';
+
+const homeBtn = document.querySelector('[data-homepage]');
+const libraryBtn = document.querySelector('[data-library]');
+const header = document.querySelector('header');
+const libraryDiv = document.querySelector('.header__library-container');
+const headerSearchDiv = document.querySelector('.header__homepage-container');
+const watchedBtn = document.querySelector('[data-watched]');
+const queueBtn = document.querySelector('[data-queue]');
+const libraryBtns = document.querySelectorAll('.library-container__btn');
+
+libraryBtn.addEventListener('click', libraryHeaderDisplay);
+queueBtn.addEventListener('click', activateLibraryBtn);
