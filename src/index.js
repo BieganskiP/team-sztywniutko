841fa950365d4b1da9e0e35d9c32fb81116e5ca5
EwaMazurek <ewa.mmazurek@gmail.com>
2023-02-10 14:45:35 +0100

    switching between home and my library
    and between queued/watched movies

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ console.log('test');
 // https://api.themoviedb.org/3/movie/550?api_key=bfe21f4061b2869ccff2b4c323a3a257
 
 // bfe21f4061b2869ccff2b4c323a3a257
-import { mainHeaderDisplay } from './js/header';
-import { libraryHeaderDisplay } from './js/header';
-import { activateLibraryBtn } from './js/header';
+import { displayHomePage } from './js/header';
+import { displayLibrary } from './js/header';
+import { swtichLibrary } from './js/header';
 
 const homeBtn = document.querySelector('[data-homepage]');
 const libraryBtn = document.querySelector('[data-library]');
@@ -15,6 +15,10 @@ const headerSearchDiv = document.querySelector('.header__homepage-container');
 const watchedBtn = document.querySelector('[data-watched]');
 const queueBtn = document.querySelector('[data-queue]');
 const libraryBtns = document.querySelectorAll('.library-container__btn');
+const homepageMoviesContainer = document.querySelector('.movie-container');
+const watchedMoviesContainer = document.querySelector('.watched');
+const queuedMoviesContainer = document.querySelector('.queued');
+const libraryContainer = document.querySelector('.library-container');
 
-libraryBtn.addEventListener('click', libraryHeaderDisplay);
-queueBtn.addEventListener('click', activateLibraryBtn);
+libraryBtn.addEventListener('click', displayLibrary);
+queueBtn.addEventListener('click', swtichLibrary);
--- src/js/header.js
+++ src/js/header.js
@@ -6,43 +6,64 @@ const headerSearchDiv = document.querySelector('.header__homepage-container');
 const watchedBtn = document.querySelector('[data-watched]');
 const queueBtn = document.querySelector('[data-queue]');
 const libraryBtns = document.querySelectorAll('.library-container__btn');
+const homepageMoviesContainer = document.querySelector('.movie-container');
+const watchedMoviesContainer = document.querySelector('.watched');
+const queuedMoviesContainer = document.querySelector('.queued');
+const libraryContainer = document.querySelector('.library-container');
 
-export const mainHeaderDisplay = e => {
-  e.currentTarget.removeEventListener('click', mainHeaderDisplay);
+export const displayHomePage = e => {
+  e.currentTarget.removeEventListener('click', displayHomePage);
+  //header-style chcange: homepage version
   libraryDiv.style.display = 'none';
   headerSearchDiv.style.display = 'block';
   header.classList.remove('library');
   e.currentTarget.classList.add('nav__btn--active');
   libraryBtn.classList.remove('nav__btn--active');
-  libraryBtn.addEventListener('click', libraryHeaderDisplay);
+  libraryBtn.addEventListener('click', displayLibrary);
+  //home page display
+  homepageMoviesContainer.style.display = 'flex';
+  libraryContainer.style.display = 'none';
 };
 
-export const libraryHeaderDisplay = e => {
-  e.currentTarget.removeEventListener('click', libraryHeaderDisplay);
+export const displayLibrary = e => {
+  e.currentTarget.removeEventListener('click', displayHomePage);
+  //header style change: my library version
   header.classList.add('library');
   headerSearchDiv.style.display = 'none';
   libraryDiv.style.display = 'flex';
   e.currentTarget.classList.add('nav__btn--active');
   homeBtn.classList.remove('nav__btn--active');
-  homeBtn.addEventListener('click', mainHeaderDisplay);
+  homeBtn.addEventListener('click', displayHomePage);
   if (queueBtn.classList.contains('library-container__btn--active')) {
     queueBtn.classList.remove('library-container__btn--active');
+    libraryBtns[1].addEventListener('click', swtichLibrary);
     watchedBtn.classList.add('library-container__btn--active');
   }
+  //display library watched section
+  homepageMoviesContainer.style.display = 'none';
+  libraryContainer.style.display = 'block';
+  watchedMoviesContainer.style.display = 'flex';
+  queuedMoviesContainer.style.display = 'none';
 };
 
-export const activateLibraryBtn = e => {
-  e.currentTarget.removeEventListener('click', activateLibraryBtn);
+export const swtichLibrary = e => {
+  e.currentTarget.removeEventListener('click', swtichLibrary);
   if (e.currentTarget.classList.contains('library-container__btn--active')) {
     return;
   }
+  //display watched movies
   if (e.currentTarget === libraryBtns[0]) {
     libraryBtns[0].classList.add('library-container__btn--active');
     libraryBtns[1].classList.remove('library-container__btn--active');
-    libraryBtns[1].addEventListener('click', activateLibraryBtn);
+    libraryBtns[1].addEventListener('click', swtichLibrary);
+    watchedMoviesContainer.style.display = 'flex';
+    queuedMoviesContainer.style.display = 'none';
+    //display queued movies
   } else {
     libraryBtns[1].classList.add('library-container__btn--active');
     libraryBtns[0].classList.remove('library-container__btn--active');
-    libraryBtns[0].addEventListener('click', activateLibraryBtn);
+    libraryBtns[0].addEventListener('click', swtichLibrary);
+    watchedMoviesContainer.style.display = 'none';
+    queuedMoviesContainer.style.display = 'flex';
   }
 };
