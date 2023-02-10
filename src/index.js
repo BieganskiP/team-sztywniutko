8fc16f5e69816ed8f5ffb01ee75948d501484d0a
Iceonq <bartek.sosnowicz@gmail.com>
2023-02-10 12:50:37 +0100

    .

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ console.log('test');
 import { mainHeaderDisplay } from './js/header';
 import { libraryHeaderDisplay } from './js/header';
 import { activateLibraryBtn } from './js/header';
+import { openModal } from './js/modal';
+import { closeModal } from './js/modal';
+import { closeModalWithEscapeKey } from './js/modal';
 
 const homeBtn = document.querySelector('[data-homepage]');
 const libraryBtn = document.querySelector('[data-library]');
@@ -15,6 +18,11 @@ const headerSearchDiv = document.querySelector('.header__homepage-container');
 const watchedBtn = document.querySelector('[data-watched]');
 const queueBtn = document.querySelector('[data-queue]');
 const libraryBtns = document.querySelectorAll('.library-container__btn');
+const libraryItem = document.querySelector('.modal-open');
+const closeModalButton = document.querySelector('.modal-close');
 
 libraryBtn.addEventListener('click', libraryHeaderDisplay);
 queueBtn.addEventListener('click', activateLibraryBtn);
+libraryItem.addEventListener('click', openModal);
+closeModalButton.addEventListener('click', closeModal);
+document.addEventListener('keydown', closeModalWithEscapeKey);
