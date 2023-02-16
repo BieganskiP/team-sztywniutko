1641f66be40e538dec9bfa4e819ded357ab2e2a1
BieganskiP <bieganski1996@gmail.com>
2023-02-16 08:02:05 +0100

    refracted pagination added search by keyword

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
-// https://api.themoviedb.org/3/movie/550?api_key=bfe21f4061b2869ccff2b4c323a3a257
-
-// bfe21f4061b2869ccff2b4c323a3a257
-import { displayHomePage } from './js/header';
-import { displayLibrary } from './js/header';
-import { swtichLibrary } from './js/header';
-
-import { openModal } from './js/modal';
-import { closeModal } from './js/modal';
+import { displayLibrary, swtichLibrary } from './js/header';
 import { closeModalWithEscapeKey } from './js/modal';
-
-const homeBtn = document.querySelector('[data-homepage]');
-const libraryBtn = document.querySelector('[data-library]');
-const header = document.querySelector('header');
-const libraryDiv = document.querySelector('.header__library-container');
-const headerSearchDiv = document.querySelector('.header__homepage-container');
-const watchedBtn = document.querySelector('[data-watched]');
-const queueBtn = document.querySelector('[data-queue]');
-const libraryBtns = document.querySelectorAll('.library-container__btn');
-const watchedMoviesContainer = document.querySelector('.watched');
-const queuedMoviesContainer = document.querySelector('.queued');
-const libraryContainer = document.querySelector('.library-container');
-const loader = document.querySelector('.loader');
-
-libraryBtn.addEventListener('click', displayLibrary);
-queueBtn.addEventListener('click', swtichLibrary);
-
-document.addEventListener('keydown', closeModalWithEscapeKey);
-
 import {
   nextPageDisplay,
   prevPageDisplay,
@@ -34,10 +7,16 @@ import {
   pagination,
 } from './js/main';
 
+
+const libraryBtn = document.querySelector('[data-library]');
+const queueBtn = document.querySelector('[data-queue]');
 const nextPage = document.querySelector('.next-button');
 const prevPage = document.querySelector('.prev-button');
 const pageNumbers = document.querySelector('.page-numbers');
 
+libraryBtn.addEventListener('click', displayLibrary);
+queueBtn.addEventListener('click', swtichLibrary);
+document.addEventListener('keydown', closeModalWithEscapeKey);
 nextPage.addEventListener('click', nextPageDisplay);
 prevPage.addEventListener('click', prevPageDisplay);
 pageNumbers.addEventListener('click', selectPage);
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,61 @@
 let apiKey = 'bfe21f4061b2869ccff2b4c323a3a257';
+let base_url = 'https://api.themoviedb.org/3';
+let _ = require('lodash');
 
+const DEBOUNCE_DELAY = 300;
 const movieLibraryContainer = document.querySelector('.movie-set');
 const pageNumbers = document.querySelector('.page-numbers');
 const loader = document.querySelector('.loader');
+const searchInput = document.querySelector('.search-form__input');
 
 let currentPageNumber = 1;
 
 import { attachModal } from './modal.js';
 
+async function fetchMoviesByKeyword(keyword) {
+  const [moviesResponse, confResponse, genresResponse] = await Promise.all([
+    fetch(`${base_url}/search/movie?api_key=${apiKey}&query=${keyword}`),
+    fetch(`${base_url}/configuration?api_key=${apiKey}`),
+    fetch(`${base_url}/genre/movie/list?api_key=${apiKey}`),
+  ]);
+
+  if (!moviesResponse.ok) {
+    const message = `An error has occured: ${moviesResponse.status}`;
+    throw new Error(message);
+  }
+  if (!confResponse.ok) {
+    const message = `An error has occured: ${confResponse.status}`;
+    throw new Error(message);
+  }
+  if (!genresResponse.ok) {
+    const message = `An error has occured: ${genresResponse.status}`;
+    throw new Error(message);
+  }
+  loader.style.display = 'none';
+  const movies = await moviesResponse.json();
+
+  const configuration = await confResponse.json();
+  const genres = await genresResponse.json();
+  return [movies, configuration.images, genres.genres];
+}
+searchInput.addEventListener(
+  'input',
+  _.debounce(() => {
+    if (searchInput.value == '') {
+      return;
+    } else {
+      fetchMoviesByKeyword(searchInput.value)
+        .then(([movies, configuration, genres]) => {
+          showMovieList(movies, configuration, genres);
+        })
+        .catch(error => {
+          console.error(
+            'movies or categories request failed. Error: ' + error.message
+          );
+        });
+    }
+  }, DEBOUNCE_DELAY)
+);
 function mapGenreIdsToName(ids, map) {
   let result = [];
   ids.forEach(id => {
@@ -22,10 +70,10 @@ async function fetchMoviesAndCategories(page) {
   loader.style.display = 'flex';
   const [moviesResponse, confResponse, genresResponse] = await Promise.all([
     fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`
+      `${base_url}/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`
     ),
-    fetch(`https://api.themoviedb.org/3/configuration?api_key=${apiKey}`),
-    fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`),
+    fetch(`${base_url}/configuration?api_key=${apiKey}`),
+    fetch(`${base_url}/genre/movie/list?api_key=${apiKey}`),
   ]);
 
   if (!moviesResponse.ok) {
@@ -61,6 +109,7 @@ export function pagination(page) {
     });
 }
 function showMovieList(movies, configuration, genres) {
+  movieLibraryContainer.innerHTML = '';
   movies.results.forEach(movie => {
     let title = movie.title;
     let category = mapGenreIdsToName(movie.genre_ids, genres);
@@ -80,148 +129,18 @@ function showMovieList(movies, configuration, genres) {
     attachModal();
   });
 }
-
-function displayPagination(currentPageNumber) {
-  pageNumbers.innerHTML = '';
-  let pageID = currentPageNumber;
-  let total = 20;
-
-  if (currentPageNumber == 1) {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${pageID} class="page-number active">${currentPageNumber}</p>
-      <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
-      <p id=${pageID + 2} class="page-number">${currentPageNumber + 2}</p>
-      <p id=${pageID + 3} class="page-number">${currentPageNumber + 3}</p>
-      <p id=${pageID + 4} class="page-number">${currentPageNumber + 4}</p>
-      <span>...</span>
-      <p id=${total} class="page-number">${total}</p>
-    `
-    );
-  } else if (currentPageNumber == 2) {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
-      <p id=${pageID} class="page-number active">${currentPageNumber}</p>
-      <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
-      <p id=${pageID + 2} class="page-number">${currentPageNumber + 2}</p>
-      <p id=${pageID + 3} class="page-number">${currentPageNumber + 3}</p>
-      <span>...</span>
-      <p id=${total} class="page-number">${total}</p>
-      `
-    );
-  } else if (currentPageNumber == 3) {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
-      <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
-      <p id=${pageID} class="page-number active">${currentPageNumber}</p>
-      <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
-      <p id=${pageID + 2} class="page-number">${currentPageNumber + 2}</p>
-      <span>...</span>
-      <p id=${total} class="page-number">${total}</p>
-      `
-    );
-  } else if (currentPageNumber == 4) {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${pageID - 3} class="page-number">${currentPageNumber - 3}</p>
-      <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
-      <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
-      <p id=${pageID} class="page-number active">${currentPageNumber}</p>
-      <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
-      <span>...</span>
-      <p id=${total} class="page-number">${total}</p>
-      `
-    );
-  } else if (currentPageNumber == 5) {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${pageID - 3} class="page-number">${currentPageNumber - 3}</p>
-      <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
-      <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
-      <p id=${pageID} class="page-number active">${currentPageNumber}</p>
-      <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
-      <span>...</span>
-      <p id=${total} class="page-number">${total}</p>
-      `
-    );
-  } else if (currentPageNumber == total - 3) {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${1} class="page-number">${1}</p>
-      <span>...</span>
-      <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
-      <p id=${pageID} class="page-number active">${currentPageNumber}</p>
-      <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
-      <p id=${pageID + 2} class="page-number">${currentPageNumber + 2}</p>
-      <p id=${total} class="page-number">${total}</p>
-      `
-    );
-  } else if (currentPageNumber == total - 2) {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${1} class="page-number">${1}</p>
-      <span>...</span>
-      <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
-      <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
-      <p id=${pageID} class="page-number active">${currentPageNumber}</p>
-      <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
-      <p id=${total} class="page-number">${total}</p>
-      `
-    );
-  } else if (currentPageNumber == total - 1) {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${1} class="page-number">${1}</p>
-      <span>...</span>
-      <p id=${pageID - 3} class="page-number">${currentPageNumber - 3}</p>
-      <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
-      <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
-      <p id=${pageID} class="page-number active">${currentPageNumber}</p>
-      <p id=${total} class="page-number">${total}</p>
-      `
-    );
-  } else if (currentPageNumber == total) {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${1} class="page-number">${1}</p>
-      <span>...</span>
-      <p id=${pageID - 4} class="page-number">${currentPageNumber - 4}</p>
-      <p id=${pageID - 3} class="page-number">${currentPageNumber - 3}</p>
-      <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
-      <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
-      <p id=${total} class="page-number active">${total}</p>
-      `
-    );
-  } else {
-    pageNumbers.insertAdjacentHTML(
-      'beforeend',
-      `<p id=${1} class="page-number">${1}</p>
-      <span>...</span>
-      <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
-      <p id=${pageID} class="page-number active">${currentPageNumber}</p>
-      <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
-      <span>...</span>
-      <p id=${total} class="page-number">${total}</p>
-      `
-    );
-  }
-}
 export function selectPage(e) {
   if (e.target.nodeName !== 'P') {
     return;
   }
   movieLibraryContainer.innerHTML = '';
   currentPageNumber = parseInt(e.target.id);
-  console.log(currentPageNumber);
   pagination(currentPageNumber);
 }
 export function nextPageDisplay(e) {
   e.preventDefault();
   movieLibraryContainer.innerHTML = '';
   currentPageNumber++;
-  console.log(currentPageNumber);
   pagination(currentPageNumber);
 }
 export function prevPageDisplay(e) {
@@ -229,7 +148,154 @@ export function prevPageDisplay(e) {
   if (currentPageNumber > 1) {
     movieLibraryContainer.innerHTML = '';
     currentPageNumber--;
-    console.log(currentPageNumber);
     pagination(currentPageNumber);
   }
+}
+// function displayPagination(currentPageNumber) {
+//   pageNumbers.innerHTML = '';
+//   let pageID = currentPageNumber;
+//   let total = 20;
+
+//   if (currentPageNumber == 1) {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${pageID} class="page-number active">${currentPageNumber}</p>
+//       <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
+//       <p id=${pageID + 2} class="page-number">${currentPageNumber + 2}</p>
+//       <p id=${pageID + 3} class="page-number">${currentPageNumber + 3}</p>
+//       <p id=${pageID + 4} class="page-number">${currentPageNumber + 4}</p>
+//       <span>...</span>
+//       <p id=${total} class="page-number">${total}</p>
+//     `
+//     );
+//   } else if (currentPageNumber == 2) {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
+//       <p id=${pageID} class="page-number active">${currentPageNumber}</p>
+//       <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
+//       <p id=${pageID + 2} class="page-number">${currentPageNumber + 2}</p>
+//       <p id=${pageID + 3} class="page-number">${currentPageNumber + 3}</p>
+//       <span>...</span>
+//       <p id=${total} class="page-number">${total}</p>
+//       `
+//     );
+//   } else if (currentPageNumber == 3) {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
+//       <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
+//       <p id=${pageID} class="page-number active">${currentPageNumber}</p>
+//       <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
+//       <p id=${pageID + 2} class="page-number">${currentPageNumber + 2}</p>
+//       <span>...</span>
+//       <p id=${total} class="page-number">${total}</p>
+//       `
+//     );
+//   } else if (currentPageNumber == 4) {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${pageID - 3} class="page-number">${currentPageNumber - 3}</p>
+//       <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
+//       <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
+//       <p id=${pageID} class="page-number active">${currentPageNumber}</p>
+//       <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
+//       <span>...</span>
+//       <p id=${total} class="page-number">${total}</p>
+//       `
+//     );
+//   } else if (currentPageNumber == 5) {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${pageID - 3} class="page-number">${currentPageNumber - 3}</p>
+//       <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
+//       <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
+//       <p id=${pageID} class="page-number active">${currentPageNumber}</p>
+//       <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
+//       <span>...</span>
+//       <p id=${total} class="page-number">${total}</p>
+//       `
+//     );
+//   } else if (currentPageNumber == total - 3 && currentPageNumber > 7) {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${1} class="page-number">${1}</p>
+//       <span>...</span>
+//       <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
+//       <p id=${pageID} class="page-number active">${currentPageNumber}</p>
+//       <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
+//       <p id=${pageID + 2} class="page-number">${currentPageNumber + 2}</p>
+//       <p id=${total} class="page-number">${total}</p>
+//       `
+//     );
+//   } else if (currentPageNumber == total - 2 && currentPageNumber > 8) {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${1} class="page-number">${1}</p>
+//       <span>...</span>
+//       <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
+//       <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
+//       <p id=${pageID} class="page-number active">${currentPageNumber}</p>
+//       <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
+//       <p id=${total} class="page-number">${total}</p>
+//       `
+//     );
+//   } else if (currentPageNumber == total - 1 && currentPageNumber > 9) {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${1} class="page-number">${1}</p>
+//       <span>...</span>
+//       <p id=${pageID - 3} class="page-number">${currentPageNumber - 3}</p>
+//       <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
+//       <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
+//       <p id=${pageID} class="page-number active">${currentPageNumber}</p>
+//       <p id=${total} class="page-number">${total}</p>
+//       `
+//     );
+//   } else if (currentPageNumber == total && currentPageNumber > 10) {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${1} class="page-number">${1}</p>
+//       <span>...</span>
+//       <p id=${pageID - 4} class="page-number">${currentPageNumber - 4}</p>
+//       <p id=${pageID - 3} class="page-number">${currentPageNumber - 3}</p>
+//       <p id=${pageID - 2} class="page-number">${currentPageNumber - 2}</p>
+//       <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
+//       <p id=${total} class="page-number active">${total}</p>
+//       `
+//     );
+//   } else {
+//     pageNumbers.insertAdjacentHTML(
+//       'beforeend',
+//       `<p id=${1} class="page-number">${1}</p>
+//       <span>...</span>
+//       <p id=${pageID - 1} class="page-number">${currentPageNumber - 1}</p>
+//       <p id=${pageID} class="page-number active">${currentPageNumber}</p>
+//       <p id=${pageID + 1} class="page-number">${currentPageNumber + 1}</p>
+//       <span>...</span>
+//       <p id=${total} class="page-number">${total}</p>
+//       `
+//     );
+//   }
+// }
+function displayPagination(currentPageNumber) {
+  const total = 20;
+  const pageNumbersArr = [];
+
+  for (let i = 1; i <= total; i++) {
+    const pageNumber = `<p id="${i}" class="page-number ${
+      i === currentPageNumber ? 'active' : ''
+    }">${i}</p>`;
+    if (
+      i === 1 ||
+      i === total ||
+      (i >= currentPageNumber - 2 && i <= currentPageNumber + 2)
+    ) {
+      pageNumbersArr.push(pageNumber);
+    } else if (i === currentPageNumber - 3 || i === currentPageNumber + 3) {
+      pageNumbersArr.push('<span>...</span>');
+    }
+  }
+
+  pageNumbers.innerHTML = pageNumbersArr.join('');
 }
