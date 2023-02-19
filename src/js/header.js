ca738ca733c932bcced52fc6334040c2dc84b1fd
BieganskiP <bieganski1996@gmail.com>
2023-02-19 14:04:46 +0100

    s

--- src/js/header.js
+++ src/js/header.js
@@ -12,6 +12,14 @@ const queuedMoviesContainer = document.querySelector('.queued');
 const libraryContainer = document.querySelector('.library-container');
 const nothingWatched = document.querySelector('.library__starter');
 
+const localStorageQueued = localStorage.getItem('queueList');
+const localStorageQueuedParsed = JSON.parse(localStorageQueued);
+
+const localStorageWatched = localStorage.getItem('watchedList');
+const localStorageWatchedParsed = JSON.parse(localStorageWatched);
+
+import { fetchMovieById } from './modal.js';
+
 export const displayHomePage = e => {
   e.currentTarget.removeEventListener('click', displayHomePage);
   //header-style chcange: homepage version
@@ -45,7 +53,6 @@ export const displayLibrary = e => {
   libraryContainer.style.display = 'block';
   watchedMoviesContainer.style.display = 'flex';
   queuedMoviesContainer.style.display = 'none';
-  paginationLibrary();
 };
 
 export const swtichLibrary = e => {
@@ -60,6 +67,7 @@ export const swtichLibrary = e => {
     libraryBtns[1].addEventListener('click', swtichLibrary);
     watchedMoviesContainer.style.display = 'flex';
     queuedMoviesContainer.style.display = 'none';
+
     //display queued movies
   } else {
     libraryBtns[1].classList.add('library-container__btn--active');
@@ -70,34 +78,10 @@ export const swtichLibrary = e => {
   }
 };
 
-import { fetchMovieById } from './modal.js';
-
-const localStorageWatched = localStorage.getItem('watchedList');
-
-const localStorageWatchedParsed = JSON.parse(localStorageWatched);
-
-export function paginationLibrary() {
-  if (localStorageWatchedParsed != null) {
-    localStorageWatchedParsed.forEach(id => {
-      fetchMovieById(id)
-        .then(([movie, configuration]) => {
-          showWatchedList(movie, configuration);
-          displayPaginationLibrary(currentPageNumber);
-        })
-        .catch(error => {
-          console.error(
-            'movies or categories request failed. Error: ' + error.message
-          );
-        });
-    });
-  } else return;
-}
-function showWatchedList(movie, configuration) {
-  nothingWatched.style.display = 'none';
+function showWatchedMovieList(movie, configuration) {
   let title = movie.title;
   let category = [];
   movie.genres.forEach(genre => category.push(genre.name));
-
   let year = movie.release_date.slice(0, 4);
   let poster = configuration.images.base_url + 'w500' + movie.poster_path;
   watchedMoviesContainer.insertAdjacentHTML(
@@ -112,54 +96,57 @@ function showWatchedList(movie, configuration) {
   </li>`
   );
 }
-
-const currentPageNumber = 1;
-const pageNumberLibrary = document.querySelector('.page-numbers-library');
-function displayPaginationLibrary(currentPageNumber) {
-  const totalCalc = watchedMoviesContainer.childElementCount - 1;
-  let total = Math.round(totalCalc / 20);
-  if (total < 1) {
-    total = 1;
-  }
-  const pageNumbersArr = [];
-
-  for (let i = 1; i <= total; i++) {
-    const pageNumber = `<p id="${i}" class="page-number ${
-      i === currentPageNumber ? 'active' : ''
-    }">${i}</p>`;
-    if (
-      i === 1 ||
-      i === total ||
-      (i >= currentPageNumber - 2 && i <= currentPageNumber + 2)
-    ) {
-      pageNumbersArr.push(pageNumber);
-    } else if (i === currentPageNumber - 3 || i === currentPageNumber + 3) {
-      pageNumbersArr.push('<span>...</span>');
-    }
-  }
-
-  pageNumberLibrary.innerHTML = pageNumbersArr.join('');
+function showQueuedMovieList(movie, configuration) {
+  let title = movie.title;
+  let category = [];
+  movie.genres.forEach(genre => category.push(genre.name));
+  let year = movie.release_date.slice(0, 4);
+  let poster = configuration.images.base_url + 'w500' + movie.poster_path;
+  queuedMoviesContainer.insertAdjacentHTML(
+    'beforeend',
+    `
+  <li class="movie-card" data-movie-id="${movie.id}" modal-open>
+    <img class="movie-card__img" src="${poster}" alt"${title}"/>
+    <div class="movie-card__description">
+      <h2 class="movie-card__description--title">${title}</h2>
+      <p class="movie-card__description--category">${category} | ${year}</p>
+    </div>
+  </li>`
+  );
 }
-
-export function selectPageLibrary(e) {
-  if (e.target.nodeName !== 'P') {
-    return;
-  }
-  watchedMoviesContainer.innerHTML = '';
-  currentPageNumber = parseInt(e.target.id);
-  paginationLibrary(currentPageNumber);
-}
-export function nextPageDisplayLibrary(e) {
-  e.preventDefault();
-  watchedMoviesContainer.innerHTML = '';
-  currentPageNumber++;
-  paginationLibrary(currentPageNumber);
-}
-export function prevPageDisplayLibrary(e) {
-  e.preventDefault();
-  if (currentPageNumber > 1) {
+function displayWatched() {
+  if (localStorageWatchedParsed != null) {
     watchedMoviesContainer.innerHTML = '';
-    currentPageNumber--;
-    paginationLibrary(currentPageNumber);
-  }
+    localStorageWatchedParsed.forEach(id => {
+      fetchMovieById(id)
+        .then(([movie, configuration]) => {
+          showWatchedMovieList(movie, configuration);
+          console.log(localStorageWatchedParsed);
+        })
+        .catch(error => {
+          console.error(
+            'movies or categories request failed. Error: ' + error.message
+          );
+        });
+    });
+  } else return;
 }
+watchedBtn.addEventListener('click', displayWatched);
+function displayQueued() {
+  if (localStorageQueuedParsed != null) {
+    queuedMoviesContainer.innerHTML = '';
+    localStorageQueuedParsed.forEach(id => {
+      fetchMovieById(id)
+        .then(([movie, configuration]) => {
+          showQueuedMovieList(movie, configuration);
+          console.log(localStorageQueuedParsed);
+        })
+        .catch(error => {
+          console.error(
+            'movies or categories request failed. Error: ' + error.message
+          );
+        });
+    });
+  } else return;
+}
+queueBtn.addEventListener('click', displayQueued);
